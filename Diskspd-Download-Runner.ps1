041db374c6912273ca4331181305b92c940b5f1a
Charlie <65982085+cecacodes@users.noreply.github.com>
2022-04-04 11:23:20 +0100

    Update Diskspd-Download-Runner.ps1

--- Diskspd-Download-Runner.ps1
+++ Diskspd-Download-Runner.ps1
@@ -23,17 +23,17 @@ if (-not(Test-Path -Path $file -PathType Container)) {
 
 Write-Host 'Command information can be found here: https://docs.microsoft.com/en-us/azure-stack/hci/manage/diskspd-overview'-ForegroundColor Green
 Write-Host 'For this test w50 (-w50 is equivalent to 50% read and 50% write'-ForegroundColor Green
-Write-Host 'Runing command DiskSpd -t2 -o32 -b4k -r4k -w50 -d5 -Sh -D -L -c1G C:\ProgramData\Kofax\TS\IO.dat > C:\$env:computername-DiskspdResults.txt' -ForegroundColor Green
+Write-Host 'Runing command DiskSpd -t2 -o32 -b4k -r4k -w50 -d5 -Sh -D -L -c1G C:\IO.dat > C:\$env:computername-DiskspdResults.txt' -ForegroundColor Green
 
-C:\DISKSPD-Ready\amd64\DiskSpd -t2 -o32 -b4k -r4k -w50 -d120 -Sh -D -L -c1G C:\ProgramData\Kofax\TS\IO.dat > C:\$env:computername-DiskspdResults.txt
+C:\DISKSPD-Ready\amd64\DiskSpd -t2 -o32 -b4k -r4k -w50 -d120 -Sh -D -L -c1G C:\IO.dat > C:\$env:computername-DiskspdResults.txt
 
 Write-Host 'Clean up time...' -ForegroundColor Green
 
-Remove-Item 'C:\ProgramData\Kofax\TS\IO.dat'
-Write-Host 'Deleted C:\ProgramData\Kofax\TS\IO.dat ' -ForegroundColor Yellow
+Remove-Item 'C:\IO.dat'
+Write-Host 'Deleted C:\IO.dat ' -ForegroundColor Yellow
 Remove-Item 'c:\DiskSpd-2.0.21a.zip'
 Write-Host 'Deleted c:\DiskSpd-2.0.21a.zip ' -ForegroundColor Yellow
 Remove-Item 'C:\DISKSPD-Ready' -Recurse
 Write-Host 'Deleted C:\DISKSPD-Ready ' -ForegroundColor Yellow
 
-Write-Host 'All Done, please attach computername-DiskspdResults.txt to your Kofax support case, so we can review ' -ForegroundColor Green
+Write-Host 'All Done, please attach computername-DiskspdResults.txt to your support case, so we can review ' -ForegroundColor Green
